aa17cbf55785af6c3ae86b7546f1abb97cceca26
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-02 18:23:59 +0900

    20230502 NEW

---
+++ src/main/kotlin/net/konohana/sakuya/maihime/handler/ForestRSRequestIndexHandler.kt
@@ -0,0 +1,34 @@
+package net.konohana.sakuya.maihime.handler
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.Resource
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+/**
+ * ## Indexハンドラクラス
+ * * ForestRS(栗州総線(鉤坂連絡移管))
+ * * 乗車券要求画面
+ */
+@Component
+class ForestRSRequestIndexHandler {
+    @Value("classpath:/request/forestrs/index.html")
+    private lateinit var forestRSRequestIndexHtml: Resource
+
+    @Bean
+    fun forestRSRequestIndexRoutes(): RouterFunction<ServerResponse> {
+        return RouterFunctions.route(RequestPredicates.GET("/forestrs/request/**")) { get(it) }
+    }
+
+    @Suppress("UnusedPrivateMember")
+    fun get(request: ServerRequest): Mono<ServerResponse> {
+        return ServerResponse.ok().contentType(MediaType.TEXT_HTML).bodyValue(forestRSRequestIndexHtml)
+    }
+}
